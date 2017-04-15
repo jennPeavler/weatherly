118eb2d0cabdf8a5a7b77e7a64b896ec3c922846
Jennifer Peavler <jenn.peavler@gmail.com>
2017-04-15 10:26:29 -0600

    begin writing more tests

--- test/App.test.js
+++ test/App.test.js
@@ -3,10 +3,17 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 
 describe('App', () => {
+  it('should start with no location', () => {
+    let app = new App;
+    expect(app.state.location).toEqual(''); 
+  } )
+
+
+
   it('should have a greeting', () => {
     let wrapper = shallow(<App />)
     let greeting = wrapper.find('#greeting')
-    
+
     expect(greeting.props().children).toEqual('Good Afternoon');
   })
 })
