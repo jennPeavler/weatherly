d0866ba45223f730a1975d232dbb6de2bc41b2e7
Jennifer Peavler <jenn.peavler@gmail.com>
2017-04-15 11:52:51 -0600

    set up localStorage and $ for jest tests, 4 passing tests

--- test/App.test.js
+++ test/App.test.js
@@ -2,18 +2,37 @@ import App from '../lib/components/App'
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 
+let app;
+let wrapperShallow;
+let wrapperMount;
+
 describe('App', () => {
+  beforeEach(() => {
+    app = new App;
+    wrapperShallow = shallow(<App />)
+    wrapperMount = mount(<App />)
+  })
+
   it('should start with no location', () => {
-    let app = new App;
-    expect(app.state.location).toEqual(''); 
+    expect(app.state.location).toEqual('');
   } )
 
-
+  it('should start without weather data', () => {
+    expect(app.state.weather).toEqual({});
+  } )
 
   it('should have a greeting', () => {
-    let wrapper = shallow(<App />)
-    let greeting = wrapper.find('#greeting')
+    let greeting = wrapperShallow.find('#greeting')
 
     expect(greeting.props().children).toEqual('Good Afternoon');
+  })
+
+  it('should submit a city for which to find the weather', () => {
+    let searchField = wrapperMount.find('input[type="text"]')
+    let submitButton = wrapperMount.find('input[type="submit"]')
+
+    searchField.simulate('change', {target: {value: 'Chicago, IL'}})
+    expect(searchField.props().value).toEqual('Chicago, IL')
+
   })
 })
